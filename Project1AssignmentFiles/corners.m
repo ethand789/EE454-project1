30c2639f44254636e3684eb14658bbb2eb5651dc
Emmanuel <ezh5373@psu.edu>
2024-02-16 18:29:38 -0500

    Fixed imfilter

--- Project1AssignmentFiles/corners.m
+++ Project1AssignmentFiles/corners.m
@@ -1,12 +1,11 @@
-function [] = corners(filename)
+function [] = corners()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-[file,S,N,D,M] = read_corner_parameters(filename);
-i = grayscale(filename);
+[file,S,N,D,M] = read_corner_parameters("cornerparams.dat");
+i = grayscale(file);
 gaus = new_gausian(S);
 i_smooth = imfilter(i,gaus);
 imagesc(i_smooth);
 colormap(gray);
-
 
 end
