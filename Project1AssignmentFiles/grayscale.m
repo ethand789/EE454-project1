b2a73ab1290c7d5cc74064374b31d8d1f83da3b1
Emmanuel <ezh5373@psu.edu>
2024-02-16 17:09:42 -0500

    Fixed to do raw grayscale

--- Project1AssignmentFiles/grayscale.m
+++ Project1AssignmentFiles/grayscale.m
@@ -1,6 +1,7 @@
-function [i] = grayscale(file)
+function [gIm] = grayscale(file)
 %return a matrix contain the intensity value of coresponding pixels from
 %file
-I=imread(file);
-i=rgb2gray(I);
+Im = imread(file);
+Im = double(Im);
+gIm =(Im(:,:,1) + Im(:,:,2) + Im(:,:,3))/3;
 end
