f8221ddaee28bd6773699ba8234b0cf00481f92a
Emmanuel <ezh5373@psu.edu>
2024-02-16 17:56:14 -0500

    format

--- Project1AssignmentFiles/corners.m
+++ Project1AssignmentFiles/corners.m
@@ -2,10 +2,11 @@ function [] = corners(filename)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 [file,S,N,D,M] = read_corner_parameters(filename);
-i= grayscale(file);
-gaus= new_gausian(S);
-i_smooth=imfilter(i,gaus);
-imshow(i);
+i = grayscale(filename);
+gaus = new_gausian(S);
+i_smooth = imfilter(i,gaus);
+imagesc(i_smooth);
+colormap(gray);
 
 
 end
