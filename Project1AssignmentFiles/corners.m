5d8f41eed58916c2c06be5b045851c672a7eaeae
Emmanuel <ezh5373@psu.edu>
2024-02-16 20:18:01 -0500

    Step 4 finished

--- Project1AssignmentFiles/corners.m
+++ Project1AssignmentFiles/corners.m
@@ -2,10 +2,23 @@ function [] = corners()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 [file,S,N,D,M] = read_corner_parameters("cornerparams.dat");
-i = grayscale(file);
+
+
+%step 2: converting image to a grayscale
+I = grayscale(file);
+
+% step 3: smoothing the image
 gaus = new_gausian(S);
-i_smooth = imfilter(i,gaus);
-imagesc(i_smooth);
+I_smooth = conv2(I,gaus);
+imagesc(I_smooth);
 colormap(gray);
 
+% step 4: computing gradients Ix and Iy
+filtx = [1/2, 0, -1/2];
+Ix = conv2(I_smooth, filtx);
+imagesc(Ix);
+
+filty = filtx';
+Iy = conv2(I_smooth, filty);
+imagesc(Iy);
 end
